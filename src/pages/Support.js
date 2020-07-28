995ec692f76197d00ebf2d6231a2562984c67718
Aladin <aladinbilalagic@gmail.com>
2020-07-28 11:13:08 +0200

    href fix

--- src/pages/Support.js
+++ src/pages/Support.js
@@ -9,9 +9,9 @@ class Support extends Component {
 
             <h5>Support for games:</h5>
             <ul>
-            <li><a href="https://apps.apple.com/us/app/swifty-ball-3d/id1510668418" style={{fontSize: '1em', color: '#00cdcd'}}>SpinLeague.io</a></li>
+            <li><a href="https://apps.apple.com/us/developer/aladin-bilalagic/id1463842782" style={{fontSize: '1em', color: '#00cdcd'}}>SpinLeague.io</a></li>
             <li><a href="https://apps.apple.com/us/app/fire-cannon-3d/id1517233502" style={{fontSize: '1em', color: '#00cdcd'}}>Fire Cannon 3D</a></li>
-            <li><a href="https://apps.apple.com/us/developer/aladin-bilalagic/id1463842782" style={{fontSize: '1em', color: '#00cdcd'}}>Swifty Ball 3D</a></li>
+            <li><a href="https://apps.apple.com/us/app/swifty-ball-3d/id1510668418" style={{fontSize: '1em', color: '#00cdcd'}}>Swifty Ball 3D</a></li>
             </ul>
             <br />
           
